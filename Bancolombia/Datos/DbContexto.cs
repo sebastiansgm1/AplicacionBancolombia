1b0f56c06bfa62462aecca1f576fbff95e9eb7bd
Sebastian Galeano <sebastiansgm1@gmail.com>
2015-10-04 16:27:46 -0500

    Creacion de tabla Usuario, con numero de cuenta autoincrementable y nombre de usuario

---
+++ Bancolombia/Datos/DbContexto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using System.Net;
+using System.Windows;
+
+
+namespace Datos
+{
+    public class DbContexto : DataContext
+    {
+        public DbContexto(string connectionString)
+            :base(connectionString)
+        {
+
+        }
+
+        public Table<TablaUsuario> Usuarios
+        {
+            get
+            {
+                return this.GetTable<TablaUsuario>
+            }
+        }
+    }
+}
---
+++ Bancolombia/Datos/TablaUsuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data.Linq.Mapping;
+using System.Data.Linq;
+
+namespace Datos
+{
+    [Table(Name = "Tusuario")] //Definicion de la clase como tabla
+
+    public class TablaUsuario
+    {
+        [Column(IsPrimaryKey = true, IsDbGenerated = true)] //Creacion de columna y caracteristicas
+        public int Cuenta { get; set; } // Definicion de columnas y atributos set para definir y get para extraer datos
+
+        [Column(CanBeNull =false)]
+        public string NombreUsuario { get; set; }
+    }
+
+}
