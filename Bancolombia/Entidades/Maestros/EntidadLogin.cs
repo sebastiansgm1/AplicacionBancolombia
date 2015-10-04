67871185b5c899235300765d2c685de068d51e8b
Sebastian Galeano <sebastiansgm1@gmail.com>
2015-10-04 16:49:09 -0500

    no message

---
+++ Bancolombia/Entidades/Maestros/EntidadLogin.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades.Maestros
+{
+    public class EntidadLogin
+    {
+        #region Atributos
+
+        /// <summary>
+        /// Corresponde a la cuenta de usuario
+        /// </summary>  
+        /// 
+
+        private Int16 _CuentaUsuario;
+
+        /// <summary>
+        /// Corresponde al nombre con el que se identifica el usuario
+        /// </summary>
+        private string _NombreUsuario;
+
+
+        #endregion
+
+        #region Propiedades
+
+        public Int16 CuentaUsuario
+        {
+            get
+            {
+                return _CuentaUsuario;
+            }
+            set
+            {
+                _CuentaUsuario = value;
+            }
+        }
+
+        public string NombreUsuario
+        {
+            get
+            {
+                return _NombreUsuario;
+            }
+            set
+            {
+                _NombreUsuario = value;
+            }
+        }
+
+        #endregion
+    }
+}
